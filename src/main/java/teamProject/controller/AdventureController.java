3386a36008fa053b06ac96c872d3c16358bca3ed
Tzortzis Kapellas <tzortziskap@hotmail.com>
2021-03-12 15:18:35 +0200

    play with controller

--- src/main/java/teamProject/controller/AdventureController.java
+++ src/main/java/teamProject/controller/AdventureController.java
@@ -5,20 +5,42 @@
  */
 package teamProject.controller;
 
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import teamProject.entity.City;
+import teamProject.entity.Event;
+import teamProject.service.CountyService;
+import teamProject.service.TypeIndoorOutdoorService;
 /**
  *
  * @author tzortziskapellas
  */
 
-@RequestMapping("/Adventure")
+@RequestMapping("/")
 @Controller
 public class AdventureController {
     
-    @GetMapping
-    public String showHome(){
+    @Autowired
+    private CountyService countyService;
+    @Autowired
+    private TypeIndoorOutdoorService indoorOutdoorService;
+    
+    @GetMapping()
+    public String showHome(Model model){
+        model.addAttribute("counties", countyService.getCountys());
+        model.addAttribute("indoorOutdoors", indoorOutdoorService.getTypeIndoorOutdoors());
+        return "index";
+    }
+    
+    @GetMapping("/find")
+    public String returnCities(@PathVariable("id") int id, Model model){
+        List<City> cities = countyService.getCountyById(id).getCityList();
+        model.addAttribute("cities", cities);
         return "index";
     }
     
